172f16d4dca3d8e490a6a1d7f457900ee5df3a2a
Charles Craig <ccraig13@gmail.com>
2014-07-14 11:18:31 -0400

    using go's methods for serving a directory

--- go-serve-this.go
+++ go-serve-this.go
@@ -3,29 +3,14 @@ package main
 import (
   "log"
   "net/http"
-  "io"
-  "os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-  filename := r.URL.Path[1:]
-
-  if filename == "" {
-    filename = "index.html"
-  }
-
-  file, err := os.Open( filename )
-
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-
-  io.Copy(w, file)
-}
-
 func main() {
-  http.HandleFunc("/", handler)
-  log.Println("Serving on port 8080...")
-  http.ListenAndServe(":8080", nil)
+  fs := http.FileServer(http.Dir("."))
+  http.Handle("/", fs)
+  log.Println("Listening...")
+  err := http.ListenAndServe(":8080", nil)
+  if err != nil {
+    log.Fatal("ListenAndServe: ", err)
+  }
 }
